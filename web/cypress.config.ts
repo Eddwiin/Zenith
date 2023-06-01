e0334e8edc47432b7bac2c16aba70807380c4713
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-06-01 12:12:15 +0200

    fix error cypress config

--- web/cypress.config.ts
+++ web/cypress.config.ts
@@ -1,13 +1,27 @@
-import { defineConfig } from 'cypress'
+import { addCucumberPreprocessorPlugin } from "@badeball/cypress-cucumber-preprocessor";
+import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+import * as createBundler from "@bahmutov/cypress-esbuild-preprocessor";
+import { defineConfig } from "cypress";
 
 export default defineConfig({
-  // setupNodeEvents can be defined in either
-  // the e2e or component configuration
   e2e: {
     specPattern: "**/*.feature",
-    setupNodeEvents(on, config) {
-      on('file:preprocessor', (file) => { })
+    async setupNodeEvents(
+      on: Cypress.PluginEvents,
+      config: Cypress.PluginConfigOptions
+    ): Promise<Cypress.PluginConfigOptions> {
+      // This is required for the preprocessor to be able to generate JSON reports after each run, and more,
+      await addCucumberPreprocessorPlugin(on, config);
+
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      // Make sure to return the config object as it might have been modified by the plugin.
+      return config;
     },
   },
-})
-
+});
