24e8e688d8220823eeae2b2cb18367c0edf6dcdb
Maguran-Effy <maguran.mahendrarajan@effy.fr>
2023-07-07 16:12:10 +0200

    configure auth

---
+++ api/src/main/java/com/zenith/api/controller/AuthentificationController.java
@@ -0,0 +1,30 @@
+package com.zenith.api.controller;
+
+import com.zenith.api.request.AuthenticationRequest;
+import com.zenith.api.request.RegisterRequest;
+import com.zenith.api.service.AuthenticationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/auth")
+@RequiredArgsConstructor
+public class AuthentificationController {
+    private final AuthenticationService authenticationService;
+    @PostMapping("/save")
+    public ResponseEntity<AuthenticationResponse> register(
+            @RequestBody RegisterRequest request
+    ) {
+        return ResponseEntity.ok(authenticationService.register());
+    }
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticate(
+            @RequestBody AuthenticationRequest request
+    ) { }
+
+}
