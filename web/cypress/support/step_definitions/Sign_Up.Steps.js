9c6ebdc5ceb42f73d241bb583c7d4ea810f83b94
Eddwiin <maguran.mahendrarajan@gmail.com>
2023-10-10 11:04:04 +0200

    setup step_definitions with cucumber + cypress

---
+++ web/cypress/support/step_definitions/Sign_Up.Steps.js
@@ -0,0 +1,15 @@
+const { Given, When, Then } = require("@badeball/cypress-cucumber-preprocessor");
+
+Given('I navigate to the sign up page', () => {})
+
+When('I type a first name with {}', (firstName) => {})
+
+When('I type a last name with {}', (lastName) => {})
+
+When('I type an email with {}', (email) => {})
+
+When('I type a password with {}', (password) => {})
+
+When('I type a confirmation password with {}', (confirmationPassword) => {})
+
+Then('I should display a message with {}', (expectedMessage) => {})
